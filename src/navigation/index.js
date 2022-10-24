7b4aeb73ce5c66be127e60d4d0b9383f692df226
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-10-24 20:22:32 +0600

    added bottom stack navigation icon

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,3 +1,8 @@
+import {
+    Ionicons,
+    MaterialCommunityIcons,
+    SimpleLineIcons
+} from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -9,6 +14,7 @@ import Earphones from "../screens/Earphones";
 import Headphones from "../screens/Headphones";
 import Home from "../screens/Home";
 import Speakers from "../screens/Speakers";
+import { colors } from "../theme/colors";
 
 const theme = {
   ...DefaultTheme,
@@ -67,35 +73,110 @@ function CartStackScreens() {
   );
 }
 
+const TabBarIcon = ({ fontFamily, name, color }) => {
+  if (fontFamily === "MaterialCommunityIcons") {
+    return <MaterialCommunityIcons name={name} size={24} color={color} />;
+  } else if (fontFamily === "Ionicons") {
+    return <Ionicons name={name} size={24} color={color} />;
+  } else if (fontFamily === "SimpleLineIcons") {
+    return <SimpleLineIcons name={name} size={24} color={color} />;
+  }
+};
+
 const Tab = createBottomTabNavigator();
 const Navigation = () => {
   return (
     <>
       <NavigationContainer theme={theme}>
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: colors.primary,
+          }}
+        >
           <Tab.Screen
-            options={{ title: "Home" }}
+            options={{
+              title: "Home",
+              tabBarIcon: ({ color }) => (
+                <TabBarIcon
+                  fontFamily={"MaterialCommunityIcons"}
+                  name="home"
+                  color={color}
+                />
+              ),
+            }}
             name="Main"
             component={HomeStackScreen}
           />
           <Tab.Screen
-            options={{ title: "Headphones" }}
+            options={{
+              title: "Headphones",
+              tabBarIcon: ({ color }) => (
+                <TabBarIcon
+                  fontFamily={"MaterialCommunityIcons"}
+                  name="headphones"
+                  color={color}
+                />
+              ),
+            }}
             name="HeadphonesTab"
             component={HeadphonesStackScreens}
           />
           <Tab.Screen
-            options={{ title: "Earphones" }}
+            options={{
+              title: "Earphones",
+              tabBarIcon: ({ color }) => (
+                <TabBarIcon
+                  fontFamily={'SimpleLineIcons'}
+                  name="earphones-alt"
+                  color={color}
+                />
+              ),
+            }}
             name="EarphonesTab"
             component={EarphonesStackScreens}
           />
           <Tab.Screen
-            options={{ title: "Speakers" }}
+            options={{
+              title: "Speakers",
+              tabBarIcon: ({ color }) => (
+                <TabBarIcon
+                  fontFamily={"MaterialCommunityIcons"}
+                  name="speaker"
+                  color={color}
+                />
+              ),
+            }}
             name="SpeakersTab"
             component={SpeakersStackScreens}
           />
           <Tab.Screen
-            options={{ title: "Cart" }}
+            options={{
+              title: "Cart",
+              tabBarIcon: ({ color }) => (
+                <TabBarIcon
+                  fontFamily={'Ionicons'}
+                  name="cart-outline"
+                  color={color}
+                />
+              ),
+            }}
             name="CartTab"
+            component={CartStackScreens}
+          />
+          <Tab.Screen
+            options={{
+              title: "Cartt",
+              tabBarIcon: ({ color }) => (
+                <TabBarIcon
+                  fontFamily={'Ionicons'}
+                  name="cart-outline"
+                  color={color}
+                />
+              ),
+            }}
+            name="CartT"
             component={CartStackScreens}
           />
         </Tab.Navigator>
